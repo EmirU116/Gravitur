6b0835d59ad8b89640e17d04bb54a40f5476a750
Emir Ulusoy <Emir.ulusoy@student.forsbergsskola.se>
2023-07-07 18:46:34 +0200

    added character texture

---
+++ Gravitur/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // WSAD movemnt
+    // Jump
+    // Interaction ( E )
+    // Gravity Manipultion ( F )
+    
+    
+}
