1a73826a2e3b61e938ef082a5c79f5d769123c5f
Emir Ulusoy <Emir.ulusoy@student.forsbergsskola.se>
2023-07-13 19:01:08 +0200

    added comments for code

--- Gravitur/Assets/TeleportingScene/Script/Teleporting.cs
+++ Gravitur/Assets/TeleportingScene/Script/Teleporting.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 
 public class Teleporting : MonoBehaviour
 {
-    public Transform playerObject, destination;
+    public Transform playerObject, destination; // player and where the player should spawn
     public BoxCollider self;
 
-    [SerializeField] private int loopCounting;
+    [SerializeField] private int loopCounting; 
 
     private void Start()
     {
-        loopCounting = 0;
+        loopCounting = 0;   // starts the loop from 0 (just in case) 
     }
 
     private void Update()
     {
+        // win state to break the loop 
         if (loopCounting >= 3)
         {
             Debug.Log("You broke the loop");
@@ -28,8 +29,8 @@ public class Teleporting : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            playerObject.position = destination.position;
-            loopCounting++;
+            playerObject.position = destination.position;   // spawns the player to destination position
+            loopCounting++; // increment 
         }
     }
 }
