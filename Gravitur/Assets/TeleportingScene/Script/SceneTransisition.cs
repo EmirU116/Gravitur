16192914b00d4e61398c906cd2a881d80dc2f6fa
Emir Ulusoy <Emir.ulusoy@student.forsbergsskola.se>
2023-07-13 19:01:16 +0200

    Update SceneTransisition.cs

--- Gravitur/Assets/TeleportingScene/Script/SceneTransisition.cs
+++ Gravitur/Assets/TeleportingScene/Script/SceneTransisition.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 
 public class SceneTransisition : MonoBehaviour
 {
-    public GameObject GO1;
-    public GameObject GO2;
+    public GameObject GO1;  // scene 1
+    public GameObject GO2;  // scene 2 
 
-    [SerializeField] private bool hasChanged;
+    [SerializeField] private bool hasChanged;   
 
     private void Start()
     {
-        GO1.SetActive(true);
+        // starting the game with first scene
+        GO1.SetActive(true);    
         GO2.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // when player is entering the collider, it will make transition from scene 1 to scene 2 quite smoothly
         if (other.CompareTag("Player"))
         {
             GO1.SetActive(false);
