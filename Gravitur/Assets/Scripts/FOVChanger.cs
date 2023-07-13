200e793636ce192aaf522ccfc9be2fae992a745a
Emir Ulusoy <Emir.ulusoy@student.forsbergsskola.se>
2023-07-13 13:20:22 +0200

    added a few book shelf ( not done though)

---
+++ Gravitur/Assets/Scripts/FOVChanger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class FOVChanger : MonoBehaviour
+{
+    public CinemachineFreeLook freeLookCamera;
+    public float startFOV = 100f;
+
+    private void Start()
+    {
+    }
+}
